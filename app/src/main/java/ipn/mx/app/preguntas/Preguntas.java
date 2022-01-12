9d81efef0ef3f435562c8589c0a1ee89c2048fc5
Cova-ops <daniel.covarrubias@xira.ai>
2022-01-11 22:09:47 -0600

    fix: New Repo

---
+++ app/src/main/java/ipn/mx/app/preguntas/Preguntas.java
@@ -0,0 +1,32 @@
+package ipn.mx.app.preguntas;
+
+public class Preguntas {
+
+    int id_preguntas;
+    String texto_pregunta;
+
+    public Preguntas(){
+
+    }
+
+    public Preguntas(int id_preguntas, String texto_pregunta) {
+        this.id_preguntas = id_preguntas;
+        this.texto_pregunta = texto_pregunta;
+    }
+
+    public int getId_preguntas() {
+        return id_preguntas;
+    }
+
+    public void setId_preguntas(int id_preguntas) {
+        this.id_preguntas = id_preguntas;
+    }
+
+    public String getTexto_pregunta() {
+        return texto_pregunta;
+    }
+
+    public void setTexto_pregunta(String texto_pregunta) {
+        this.texto_pregunta = texto_pregunta;
+    }
+}
---
+++ app/src/main/java/ipn/mx/app/usuario/Usuario.java
@@ -0,0 +1,73 @@
+package ipn.mx.app.usuario;
+
+import java.sql.Date;
+
+public class Usuario {
+
+    String correo;
+    String nombre;
+    String ap_paterno;
+    String ap_materno;
+    String password;
+    Date fecha_nacimiento;
+
+    public Usuario() {
+    }
+
+    public Usuario(String correo, String nombre, String ap_paterno, String ap_materno, String password, Date fecha_nacimiento) {
+        this.correo = correo;
+        this.nombre = nombre;
+        this.ap_paterno = ap_paterno;
+        this.ap_materno = ap_materno;
+        this.password = password;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAp_paterno() {
+        return ap_paterno;
+    }
+
+    public void setAp_paterno(String ap_paterno) {
+        this.ap_paterno = ap_paterno;
+    }
+
+    public String getAp_materno() {
+        return ap_materno;
+    }
+
+    public void setAp_materno(String ap_materno) {
+        this.ap_materno = ap_materno;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+}
